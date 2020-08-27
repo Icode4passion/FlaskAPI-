299766dd0428f95d5298c46115fd25034ea396f3
Icode4passion <39987888+Icode4passion@users.noreply.github.com>
2020-08-27 17:48:33 +0530

    first commit

--- api/bucketlist/app/__init__.py
+++ api/bucketlist/app/__init__.py
@@ -4,7 +4,11 @@
 
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy 
-from flask import request , jsonify , abort
+from flask import request , jsonify , abort , make_response
+from flask import Blueprint
+from flask.views import MethodView
+
+from flask_migrate import Migrate 
 
 
 from datetime import datetime
@@ -19,77 +23,119 @@ app = FlaskAPI(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= db_file 
 app.config['SQL_TRACK_MODIFICATIONS']= False 
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+
+auth_blueprint = Blueprint('auth',__name__)
 
 
 
+from app.models import BucketList , User
 
 
-
-class BucketList(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    topic = db.Column(db.String(255))
-    topic_by = db.Column(db.String(100))
-    created_date = db.Column(db.DateTime,nullable= False,default = datetime.now())
-
-    def __init__(self,topic,topic_by):
-        self.topic=topic
-        self.topic_by=topic_by      
-
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-
-    @staticmethod
-    def get_all():
-        return BucketList.query.all()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit
-
-    def __repr__(self):
-        return "<Bucketlist: {}>".format(self.topic)
-
-
-
+from flask_bcrypt import Bcrypt
 
 @app.route('/api/v1/bucketlist/',methods=["POST","GET"])
 def get_bucketlist():
-    if request.method == "POST":
-        topic = (request.data.get('topic',''))
-        topic_by = (request.data.get('topic_by',''))
-       
-        bucketlist = BucketList(topic=topic,topic_by=topic_by)
-        bucketlist.save()
-        response = jsonify({'id' : bucketlist.id,'topic' : bucketlist.topic,'topic_by': bucketlist.topic_by,'created_date': bucketlist.created_date })
+    #get access tocken from header
+    auth_header = request.headers.get('Authorization')
+    
+    access_token = auth_header.split(" ")[1]
+    if access_token:
+        user_id = User.decode_token(access_token)
+        if not isinstance(user_id,str):
+            if request.method == "POST":
+                topic = str(request.data.get('topic',''))
+                topic_by = str(request.data.get('topic_by',''))
+                if topic:
+                    bucketlist = BucketList(topic=topic,topic_by=topic_by,created_by=user_id)
+                    bucketlist.save()
+                    response = jsonify({'id' : bucketlist.id,'topic' : bucketlist.topic,
+                            'topic_by': bucketlist.topic_by,'created_date': bucketlist.created_date,
+                            'date_modified':bucketlist.date_modified , 'created_by': user_id})         
+                    return make_response(response) , 201
 
-        response.status_code = 201
-        return response
-
-    else:
-        bucketlists = BucketList.get_all()
-        results = []
-        for bucketlist in bucketlists : 
-            obj = {
-                'id' : bucketlist.id,
-                'topic' : bucketlist.topic,
-                'topic_by': bucketlist.topic_by,
-                'created_date': bucketlist.created_date
+            else:
+                bucketlists = BucketList.get_all()
+                results = []
+                for bucketlist in bucketlists :
+                    obj = {
+                        'id' : bucketlist.id,
+                        'topic' : bucketlist.topic,
+                        'topic_by': bucketlist.topic_by,
+                        'created_date': bucketlist.created_date,
+                        'date_modified':bucketlist.date_modified,
+                        'created_by': bucketlist.created_by
+                    }
+                    results.append(obj)        
+                return make_response(jsonify(results)),200
+        else :
+            message = user_id
+            response ={
+                'message': message
             }
-            results.append(obj)
-        response = jsonify(results)
-        response.status_code = 200
-        return response
-        
+            return make_response(jsonify(response)), 401
 
-    return app
+@app.route('/api/v1/bucketlist/<int:id>',methods=["PUT","GET","DELETE"])
+def modify_bucketlist(id,**kwargs):
 
+    auth_header = request.headers.get('Authorization')
+    access_token = auth_header.split(" ")[1]
+    if access_token:
+        user_id = User.decode_token(access_token)
+        if not isinstance(user_id,str):
+            bucketlist = BucketList.query.filter_by(id=id).first()
+            if not bucketlist:
+                abort(404)
 
+            if request.method == "DELETE":
+                bucketlist.delete()
+                return {
+                    "message" : "bucketlist {} deleted successfully".format(bucketlist.id)
+                        }
+    
+            elif request.method == "PUT":
+                
+                topic = str(request.data.get('topic',''))
+                topic_by = str(request.data.get('topic_by',''))
+                bucketlist.topic = topic
+                bucketlist.topic_by = topic_by
+                bucketlist.save()
 
+                response = {
+                    'id' : bucketlist.id,
+                    'topic' : bucketlist.topic,
+                    'topic_by': bucketlist.topic_by,
+                    'created_date': bucketlist.created_date,
+                    'date_modified':bucketlist.date_modified,
+                    'created_by': bucketlist.created_by
 
+                }
+         
+                return make_response(jsonify(response)), 200
+    
+            else :
 
+                response = {
+                    'id' : bucketlist.id,
+                    'topic' : bucketlist.topic,
+                    'topic_by': bucketlist.topic_by,
+                    'created_date': bucketlist.created_date,
+                    'date_modified':bucketlist.date_modified,
+                    'created_by': bucketlist.created_by
+
+                 }         
+                return make_response(jsonify(response)), 200
+        else:
+
+            message = user_id
+            response = {
+                'message': message
+                }
+                # return an error response, telling the user he is Unauthorized
+            return make_response(jsonify(response)), 401
+
+from .auth import auth_blueprint
+app.register_blueprint(auth_blueprint)
 
 
---
+++ api/bucketlist/app/models.py
@@ -0,0 +1,110 @@
+
+from app import db
+from flask_bcrypt import Bcrypt
+from flask import current_app
+import jwt
+from datetime import datetime, timedelta
+
+
+
+
+class User(db.Model):
+    """This class defines the users table """
+
+    __tablename__ = 'users'
+
+    # Define the columns of the users table, starting with the primary key
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    bucketlists = db.relationship(
+        'BucketList', order_by='BucketList.id', cascade="all, delete-orphan")
+
+    def __init__(self, email, password):
+        """Initialize the user with an email and a password."""
+        self.email = email
+        self.password = Bcrypt().generate_password_hash(password).decode()
+
+    def password_is_valid(self, password):
+        """
+        Checks the password against it's hash to validates the user's password
+        """
+        return Bcrypt().check_password_hash(self.password, password)
+
+    def save(self):
+        """Save a user to the database.
+        This includes creating a new user and editing one.
+        """
+        db.session.add(self)
+        db.session.commit()
+
+    def generate_token(self, user_id):
+        """ Generates the access token"""
+
+        try:
+            # set up a payload with an expiration time
+            payload = {
+                'exp': datetime.utcnow() + timedelta(minutes=5),
+                'iat': datetime.utcnow(),
+                'sub': user_id
+            }
+           
+            # create the byte string token using the payload and the SECRET key
+            jwt_string = jwt.encode(            
+                payload,
+                'SECRET',
+                algorithm='HS256'
+            )
+            return jwt_string
+            
+
+        except Exception as e:
+            # return an error in string format if an exception occurs
+            return str(e)
+
+    @staticmethod
+    def decode_token(token):
+        """Decodes the access token from the Authorization header."""
+        try:
+            # try to decode the token using our SECRET variable
+            payload = jwt.decode(token, 'SECRET')
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            # the token is expired, return an error string
+            return "Expired token. Please login to get a new token"
+        except jwt.InvalidTokenError:
+            # the token is invalid, return an error string
+            return "Invalid token. Please register or login"
+
+class BucketList(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String(255))
+    topic_by = db.Column(db.String(100))
+    created_date = db.Column(db.DateTime,nullable= False,default = db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+    created_by = db.Column(db.Integer, db.ForeignKey(User.id))
+
+    def __init__(self,topic,topic_by,created_by):
+        self.topic=topic
+        self.topic_by=topic_by
+        self.created_by = created_by      
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    @staticmethod
+    def get_all():
+        return BucketList.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<Bucketlist: {}>".format(self.topic)
